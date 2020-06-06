42722096761906f8c1965228e7b818708591dbed
Stijn <stijn@kolkies.dev>
2020-06-06 20:18:11 +0200

    Add code

---
+++ README.md
@@ -0,0 +1,19 @@
+# Github Packages Docker
+
+This action deploys an docker image to the github package registy
+
+## Inputs
+
+### `repo-token`
+
+**Required** Access token which is used to deploy the image to the repository. I advise to use `GITHUB_TOKEN`.
+
+### `image-name`
+
+**Required** Name for the docker image
+
+## Outputs
+
+### `url`
+
+This can be used when deploying
---
+++ index.js
@@ -0,0 +1,37 @@
+const { exec } = require("@actions/exec");
+const core = require("@actions/core");
+
+const run = async () => {
+  const githubToken = core.getInput("repo-token");
+
+  const repoUser = process.env.GITHUB_ACTOR;
+
+  const imageName = core.getInput("image-name").toLowerCase();
+
+  const githubRepo = process.env.GITHUB_REPOSITORY.toLowerCase();
+
+  const tag = "";
+
+  await exec("git describe --always --abbrev --tags --long", "", {
+    listeners: {
+      stdout: (data) => {
+        tag += data.toString();
+      }
+    }
+  });
+
+  const imageText = `docker.pkg.github.com/${githubRepo}/${imageName}`;
+
+  await exec(`docker login docker.pkg.github.com -u ${repoUser} -p ${githubToken}`).catch(error => core.setFailed(error));
+
+  await exec(`docker build -t ${imageText}:${tag} .`).catch(error => core.setFailed(error));
+
+  await exec(`docker tag ${imageText}:${tag} ${imageName}:latest`).catch(error => core.setFailed(error));
+
+
+  await exec(`docker push ${imageText}:${tag}`).catch(error => core.setFailed(error));
+
+  core.setOutput("url", `${imageText}:${tag}`);
+}
+
+run();
